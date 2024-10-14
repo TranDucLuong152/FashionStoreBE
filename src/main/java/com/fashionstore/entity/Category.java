25dbe304d7c1e699412de03824cf51831d9148d9
tranducluong <tranducluong886@gmail.com>
2024-10-14 23:17:06 +0700

    add project

---
+++ src/main/java/com/fashionstore/entity/Category.java
@@ -0,0 +1,32 @@
+package com.fashionstore.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long categoryId;
+
+    private String categoryName;
+
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<Product> products;
+}
---
+++ src/main/java/com/fashionstore/entity/Product.java
@@ -0,0 +1,48 @@
+package com.fashionstore.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+
+    private String nameProduct;
+    private BigDecimal price;
+
+    @Temporal(TemporalType.DATE)
+    private Date publicationDate;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    private String size;
+    private String color;
+    private String image;
+
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
+
